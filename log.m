f493125ec6cdf3f3f57184b5fb0940d9d39895ce
Mahm-MoodForFood <53920737+Mahm-MoodForFood@users.noreply.github.com>
2021-12-08 22:41:11 +0000

    Create log.m

---
+++ log.m
@@ -0,0 +1,6 @@
+# Development Log
+
+This is a simple development log that I can use to keep track of progress and to highlight updates and changes with the project. 
+  
+ ## 07/12/2021
+ Created the repository with the readme file and added a cointegartion file. I plan on changing things up drasticly as it is obviously not good practice to develop any piece of software in this manner. 
